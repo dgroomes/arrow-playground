d8d06fd299ebb0dbe283b4d2e3c3ca0c1e389f47
David Groomes <dwgroomes@gmail.com>
2023-01-28 18:10:52 -0600

    Iterate over the data (highest pop)

--- README.md
+++ README.md
@@ -32,6 +32,7 @@ Follow these instructions to build and run the example program.
      [main] INFO org.apache.arrow.memory.CheckAllocator - Using DefaultAllocationManager at memory-netty/10.0.1/8975307e2967474540cbc8080869767000aee1f7/arrow-memory-netty-10.0.1.jar!/org/apache/arrow/memory/DefaultAllocationManagerFactory.class
      [main] INFO dgroomes.Runner - ZIP code vector: [90210, 19106, 82190]
      [main] INFO dgroomes.Runner - Population vector: [20700, 7043, 443]
+     [main] INFO dgroomes.Runner - The highest population is 20700 in ZIP code 90210
      ```
 
 
--- src/main/java/dgroomes/Runner.java
+++ src/main/java/dgroomes/Runner.java
@@ -30,8 +30,14 @@ public class Runner {
          IntVector zipCodeVector = new IntVector("zip-codes", allocator);
          IntVector populationVector = new IntVector("populations", allocator)) {
 
-      zipCodeVector.allocateNew(3);
-      populationVector.allocateNew(3);
+      // We are modelling this many ZIP codes across the data set. We need to hang on to this number so we can initialize
+      // the vectors correctly and iterate over the data correctly (avoid going out of bounds!).
+      // Editorialization: this is a low-level trait. But that's what we get for wanting to get closer to the metal
+      // (memory).
+      int zipValuesSize = 3;
+
+      zipCodeVector.allocateNew(zipValuesSize);
+      populationVector.allocateNew(zipValuesSize);
 
       // Beverly Hills
       {
@@ -51,11 +57,24 @@ public class Runner {
         populationVector.set(2, 443);
       }
 
-      zipCodeVector.setValueCount(3);
-      populationVector.setValueCount(3);
+      zipCodeVector.setValueCount(zipValuesSize);
+      populationVector.setValueCount(zipValuesSize);
 
       log.info("ZIP code vector: {}", zipCodeVector);
       log.info("Population vector: {}", populationVector);
+
+      int highestPopulation = -1;
+      int highestPopulationIdx = -1;
+      for (int i = 0; i < zipValuesSize; i++) {
+        int pop = populationVector.get(i);
+        if (pop > highestPopulation) {
+          highestPopulation = pop;
+          highestPopulationIdx = i;
+        }
+      }
+
+      int zipCode = zipCodeVector.get(highestPopulationIdx);
+      log.info("The highest population is {} in ZIP code {}", highestPopulation, zipCode);
     }
   }
 }
